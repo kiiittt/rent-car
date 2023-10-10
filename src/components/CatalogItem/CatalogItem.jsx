ed76fa22ef389b2b79301d68fee65566b35fc37c
Daria <daryakolisnik@gmail.com>
2023-10-10 14:57:58 +0300

    to do a render cars in CatalogList

--- src/components/CatalogItem/CatalogItem.jsx
+++ src/components/CatalogItem/CatalogItem.jsx
@@ -12,44 +12,52 @@ import {
   ItemButton,
   InfoText,
 } from './CatalogItem.styled';
+import React, { useState, useEffect } from 'react';
 
 const CatalogListItem = () => {
+  const [cars, setCars] = useState([]);
+
+  useEffect(() => {
+    fetch('https://65240384ea560a22a4e9413d.mockapi.io/auto')
+      .then(response => response.json())
+      .then(data => setCars(data))
+      .catch(error => console.error('Error', error));
+  }, []);
+
   return (
     <>
-      <CatalogItem>
-        <ImgContainer>
-          <HeartIcon />
-          <ItemImg
-            src="https://cdn.forbes.ru/files/c/1082x683/photo_galleries/1920-02_tcm-3173-1834873.jpg__1582289253__55999.webp"
-            alt=""
-            width="274"
-          />
-          <GradientOverlay />
-        </ImgContainer>
-        <ItemTitle>
-          <ItemTitleName>
-            <p>Make</p>
-            <p style={{ color: '#3470ff' }}>Model,</p>
-            <p>Year</p>
-          </ItemTitleName>
-          <p>Price</p>
-        </ItemTitle>
-        <ItemInfo>
-          <ItemInfoFirst>
-            <InfoText>Location</InfoText>
-            <InfoText>Country</InfoText>
-            <InfoText>Rentals</InfoText>
-            <InfoText>Premium</InfoText>
-          </ItemInfoFirst>
-          <ItemInfoSecond>
-            <InfoText>Category</InfoText>
-            <InfoText>Model</InfoText>
-            <InfoText>Number</InfoText>
-            <InfoText>Feature</InfoText>
-          </ItemInfoSecond>
-        </ItemInfo>
-        <ItemButton>Learn more</ItemButton>
-      </CatalogItem>
+      {cars.map(car => (
+        <CatalogItem key={car.id}>
+          <ImgContainer>
+            <HeartIcon />
+            <ItemImg src={car.img} alt="" width="274" />
+            <GradientOverlay />
+          </ImgContainer>
+          <ItemTitle>
+            <ItemTitleName>
+              <p>{car.make}</p>
+              <p style={{ color: '#3470ff' }}>{car.model},</p>
+              <p>{car.year}</p>
+            </ItemTitleName>
+            <p>{car.price}</p>
+          </ItemTitle>
+          <ItemInfo>
+            <ItemInfoFirst>
+              <InfoText>Location</InfoText>
+              <InfoText>Country</InfoText>
+              <InfoText>Rentals</InfoText>
+              <InfoText>Premium</InfoText>
+            </ItemInfoFirst>
+            <ItemInfoSecond>
+              <InfoText>Category</InfoText>
+              <InfoText>Model</InfoText>
+              <InfoText>Number</InfoText>
+              <InfoText>Feature</InfoText>
+            </ItemInfoSecond>
+          </ItemInfo>
+          <ItemButton>Learn more</ItemButton>
+        </CatalogItem>
+      ))}
     </>
   );
 };
